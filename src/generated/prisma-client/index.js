a17a67df5633e7ceff50bb087addaa774d189b23
Holden B <holden.bucher.92@gmail.com>
2019-05-01 22:28:43 -0500

    lots of things

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,70 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Gender",
+    embedded: false
+  },
+  {
+    name: "Role",
+    embedded: false
+  },
+  {
+    name: "SkillLevel",
+    embedded: false
+  },
+  {
+    name: "LessonType",
+    embedded: false
+  },
+  {
+    name: "Day",
+    embedded: false
+  },
+  {
+    name: "Color",
+    embedded: false
+  },
+  {
+    name: "Student",
+    embedded: false
+  },
+  {
+    name: "AccountAdmin",
+    embedded: false
+  },
+  {
+    name: "FrontDesk",
+    embedded: false
+  },
+  {
+    name: "Instructor",
+    embedded: false
+  },
+  {
+    name: "MedicalCondition",
+    embedded: false
+  },
+  {
+    name: "Class",
+    embedded: false
+  },
+  {
+    name: "Lesson",
+    embedded: false
+  },
+  {
+    name: "AgeGroup",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eastsideswim-16e1c20ddd.herokuapp.com/eastsideswim/dev`,
+  secret: `${process.env["PRISMA_SECRET"]}`
+});
+exports.prisma = new exports.Prisma();
