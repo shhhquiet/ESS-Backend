da389b45f4ed59c62806a01ebbe444fc5f0d9367
Holden B <holden.bucher.92@gmail.com>
2019-05-09 19:16:59 -0500

    k

--- src/resolvers/query/index.js
+++ src/resolvers/query/index.js
@@ -2,17 +2,21 @@ const { forwardTo } = require('prisma-binding');
 module.exports = {
 	users: forwardTo('prisma'),
 	employees: forwardTo('prisma'),
+	lessons: forwardTo('prisma'),
+	classes: forwardTo('prisma'),
 	async currentUser(parent, args, { userId, query }, info) {
 		if (!userId) return null;
 		console.log(query);
 		return query.node({ id: userId }, info);
 	},
-	async allLessons(parent, args, { user, query }, info) {
+	async fullSchedule(parent, args, { user, query }, info) {
 		// that'll be there to block the route when we wanna block it
 		// if (!user.role === 'ADMIN') throw new Error('You must be an admin to access this data');
 		try {
-			const lessons = await query.lessons({}, info);
-			return lessons;
+			const lessons = await query.lessons();
+			const classes = await query.classes();
+
+			return { lessons, classes };
 		} catch (e) {
 			throw new Error(`Oops something bad happened: ${e}`);
 		}
