a4d11a1f7c3ab5ddfd310bc5afc7f721fdaf608b
Lauren <llwor94@gmail.com>
2019-06-09 03:12:09 -0700

    seeded lessons and did some updates

---
+++ src/db/seeds/seedLessons.js
@@ -0,0 +1,57 @@
+module.exports = [
+  {
+    "instructor": {
+      "connect": {
+        "id": "cjvexqt6u00050754u9j7hhod"
+      }
+    },
+    "duration": 30,
+    "day": "WEDNESDAY",
+    "time": "4:00",
+    "open": true
+  },
+  {
+    "instructor": {
+      "connect": {
+        "id": "cjvexqt6u00050754u9j7hhod"
+      }
+    },
+    "duration": 30,
+    "day": "WEDNESDAY",
+    "time": "4:30",
+    "open": true
+  },
+  {
+    "instructor": {
+      "connect": {
+        "id": "cjvexqt6u00050754u9j7hhod"
+      }
+    },
+    "duration": 30,
+    "day": "WEDNESDAY",
+    "time": "3:30",
+    "open": true
+  },
+  {
+    "instructor": {
+      "connect": {
+        "id": "cjvhn64z100er0776hxfeyilb"
+      }
+    },
+    "duration": 30,
+    "day": "WEDNESDAY",
+    "time": "3:30",
+    "open": true
+  },
+  {
+    "instructor": {
+      "connect": {
+        "id": "cjvhn64z100er0776hxfeyilb"
+      }
+    },
+    "duration": 30,
+    "day": "MONDAY",
+    "time": "3:30",
+    "open": true
+  }
+]
